5e6d3f3afa596fecbed50a6225b978c8edd8e4c2
SixZeroSevenNine <36509413+SixZeroSevenNine@users.noreply.github.com>
2018-03-11 19:26:41 -0400

    Update purge-unused-kernels.sh

--- bash-scripts/purge-unused-kernels.sh
+++ bash-scripts/purge-unused-kernels.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+#
+# Purges unused kernels in Ubuntu.
+#
 sudo apt-get remove --purge $(dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d')
